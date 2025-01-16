0c775a8d6594eb6c7379d646ee098c3eb2083d83
Tomas Tamantini <tomasbft@gmail.com>
2025-01-16 08:35:20 -0300

    Extract RPS player

---
+++ gamehub/games/rock_paper_scissors/player.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from gamehub.games.rock_paper_scissors.selection import RPSSelection
+from gamehub.games.rock_paper_scissors.shared_view import RPSSharedPlayerView
+
+
+@dataclass(frozen=True)
+class RPSPlayer:
+    player_id: str
+    selection: Optional[RPSSelection]
+
+    def shared_view(self) -> RPSSharedPlayerView:
+        return RPSSharedPlayerView(
+            player_id=self.player_id,
+            selected=self.selection is not None,
+        )
