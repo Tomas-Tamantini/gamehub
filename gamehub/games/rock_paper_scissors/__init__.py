664d194baa921ef162623ca02af91635500e46b7
Tomas Tamantini <tomasbft@gmail.com>
2025-01-20 12:29:41 -0300

    Remove unnecessary module imports

--- gamehub/games/rock_paper_scissors/__init__.py
+++ gamehub/games/rock_paper_scissors/__init__.py
@@ -1,23 +1,4 @@
 from gamehub.games.rock_paper_scissors.game_logic import RPSGameLogic
-from gamehub.games.rock_paper_scissors.game_state import RPSGameState
 from gamehub.games.rock_paper_scissors.move import RPSMove
-from gamehub.games.rock_paper_scissors.selection import RPSSelection
-from gamehub.games.rock_paper_scissors.views import (
-    RPSPlayerView,
-    RPSPrivateView,
-    RPSResultView,
-    RPSSharedPlayerView,
-    RPSSharedView,
-)
 
-__all__ = [
-    "RPSGameLogic",
-    "RPSGameState",
-    "RPSMove",
-    "RPSSelection",
-    "RPSPlayerView",
-    "RPSPrivateView",
-    "RPSResultView",
-    "RPSSharedPlayerView",
-    "RPSSharedView",
-]
+__all__ = ["RPSGameLogic", "RPSMove"]
--- tests/test_rock_paper_scissors.py
+++ tests/test_rock_paper_scissors.py
@@ -1,13 +1,12 @@
 import pytest
 
 from gamehub.core.exceptions import InvalidMoveError
-from gamehub.games.rock_paper_scissors import (
-    RPSGameLogic,
-    RPSMove,
+from gamehub.games.rock_paper_scissors import RPSGameLogic, RPSMove
+from gamehub.games.rock_paper_scissors.selection import RPSSelection
+from gamehub.games.rock_paper_scissors.views import (
     RPSPlayerView,
     RPSPrivateView,
     RPSResultView,
-    RPSSelection,
     RPSSharedPlayerView,
     RPSSharedView,
 )
