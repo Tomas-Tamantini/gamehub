de3431f427ae0345664d12fbe73aadccffab6e36
Tomas Tamantini <tomasbft@gmail.com>
2025-01-20 11:36:19 -0300

    Extract message handling

--- clients/rock_paper_scissors/app.js
+++ clients/rock_paper_scissors/app.js
@@ -3,54 +3,13 @@ import {
     startGameBtn,
     rockBtn,
     paperBtn,
-    scissorsBtn,
-    statusArea
+    scissorsBtn
 } from "./dom.js";
+import handleMessage from "./handle_message.js";
 import { joinGame, makeMove } from "./requests.js";
 
 
-var mySelection = "";
-
-
-socketService.subscribe(
-    msg => {
-        if (msg.message_type == "ERROR") {
-            statusArea.innerHTML = "ERROR: " + msg.payload.error;
-        }
-        else if (msg.message_type == "PLAYER_JOINED") {
-            statusArea.innerHTML = msg.payload.player_ids[0] + " joined the game";
-        }
-        else if (msg.message_type == "GAME_STATE") {
-            if (msg.payload.private_view) {
-                mySelection = msg.payload.private_view.selection;
-                statusArea.innerHTML = "Your selection: " + mySelection;
-            }
-            else if (msg.payload.shared_view) {
-                if (msg.payload.shared_view.result) {
-                    mySelection = "";
-                    if (msg.payload.shared_view.result.winner) {
-                        statusArea.innerHTML = msg.payload.shared_view.result.winner + " wins!";
-                    }
-                    else {
-                        statusArea.innerHTML = "It's a tie!";
-                    }
-                }
-                else {
-                    if (mySelection) {
-                        statusArea.innerHTML = "Waiting for other players to make their move";
-                    }
-                    else {
-                        statusArea.innerHTML = "Make your move";
-                    }
-                }
-            }
-        }
-        else {
-            statusArea.innerHTML = msg;
-        }
-    }
-)
-
+socketService.subscribe(handleMessage)
 startGameBtn.onclick = joinGame;
 rockBtn.onclick = () => makeMove("ROCK");
 paperBtn.onclick = () => makeMove("PAPER");
---
+++ clients/rock_paper_scissors/handle_message.js
@@ -0,0 +1,40 @@
+import { statusArea } from "./dom.js";
+
+var mySelection = "";
+
+export default function handleMessage(msg) {
+    if (msg.message_type == "ERROR") {
+        statusArea.innerHTML = "ERROR: " + msg.payload.error;
+    }
+    else if (msg.message_type == "PLAYER_JOINED") {
+        statusArea.innerHTML = msg.payload.player_ids[0] + " joined the game";
+    }
+    else if (msg.message_type == "GAME_STATE") {
+        if (msg.payload.private_view) {
+            mySelection = msg.payload.private_view.selection;
+            statusArea.innerHTML = "Your selection: " + mySelection;
+        }
+        else if (msg.payload.shared_view) {
+            if (msg.payload.shared_view.result) {
+                mySelection = "";
+                if (msg.payload.shared_view.result.winner) {
+                    statusArea.innerHTML = msg.payload.shared_view.result.winner + " wins!";
+                }
+                else {
+                    statusArea.innerHTML = "It's a tie!";
+                }
+            }
+            else {
+                if (mySelection) {
+                    statusArea.innerHTML = "Waiting for other players to make their move";
+                }
+                else {
+                    statusArea.innerHTML = "Make your move";
+                }
+            }
+        }
+    }
+    else {
+        statusArea.innerHTML = msg;
+    }
+}
