f9ffadd563505f6825778f79bcef0b3550fd6bd0
Tomas Tamantini <tomasbft@gmail.com>
2025-01-22 20:11:29 -0300

    Update class config to config dict

--- gamehub/games/playing_cards/playing_card.py
+++ gamehub/games/playing_cards/playing_card.py
@@ -1,13 +1,12 @@
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from gamehub.games.playing_cards.suits import Suits
 
 
 class PlayingCard(BaseModel):
+    model_config: ConfigDict = {"frozen": True}
+
     rank: Literal["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
     suit: Suits
-
-    class Config:
-        frozen = True
