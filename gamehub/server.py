5111713f05a97a5eeab4d2bf84ac5411eb42ed1b
Tomas Tamantini <tomasbft@gmail.com>
2025-02-07 13:59:34 -0300

    Remove unused import

--- gamehub/server.py
+++ gamehub/server.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from fastapi import Depends, FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
