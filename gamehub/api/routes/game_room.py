85f17b1366277e2871f171c4cfbf840e29797f99
Tomas Tamantini <tomasbft@gmail.com>
2025-03-08 10:09:33 -0300

    Extract list response type

--- gamehub/api/routes/game_room.py
+++ gamehub/api/routes/game_room.py
@@ -2,19 +2,17 @@ from http import HTTPStatus
 from typing import Optional
 
 from fastapi import APIRouter
-from pydantic import BaseModel
 
 from gamehub.api.dependencies import T_RoomManager
+from gamehub.api.dto.list_response import ListResponse
 from gamehub.core.room_state import RoomState
 
 rooms_router = APIRouter(prefix="/rooms", tags=["rooms"])
 
 
-class GetRoomsResponse(BaseModel):
-    items: list[RoomState]
-
-
-@rooms_router.get("/", status_code=HTTPStatus.OK, response_model=GetRoomsResponse)
+@rooms_router.get(
+    "/", status_code=HTTPStatus.OK, response_model=ListResponse[RoomState]
+)
 async def get_rooms(room_manager: T_RoomManager, game_type: Optional[str] = None):
     rooms = list(room_manager.room_states(game_type))
-    return GetRoomsResponse(items=rooms)
+    return ListResponse[RoomState](items=rooms)
