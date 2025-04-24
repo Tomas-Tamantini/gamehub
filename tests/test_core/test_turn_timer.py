254e6376b77e2d928c3db991e077060baea50a0a
Tomas Tamantini <tomasbft@gmail.com>
2025-04-24 20:38:11 -0300

    refactor turn timer tests

--- tests/test_core/test_turn_timer.py
+++ tests/test_core/test_turn_timer.py
@@ -21,39 +21,34 @@ def spy_timer():
     return _create_spy_timer
 
 
-def test_turn_timer_registry_resets_proper_turn_timer_on_game_start(spy_timer):
-    spy_timer_a = spy_timer(123)
-    spy_timer_b = spy_timer(456)
-    registry = TurnTimerRegistry(turn_timers=[spy_timer_a, spy_timer_b])
-    registry.handle_game_start(GameStarted(room_id=123))
-    spy_timer_a.reset.assert_called_once()
-    spy_timer_b.reset.assert_not_called()
+@pytest.fixture
+def spy_timers(spy_timer):
+    return [spy_timer(room_id=1), spy_timer(room_id=2)]
 
 
-def test_turn_timer_registry_resets_proper_turn_timer_on_game_end(spy_timer):
-    spy_timer_a = spy_timer(123)
-    spy_timer_b = spy_timer(456)
-    registry = TurnTimerRegistry(turn_timers=[spy_timer_a, spy_timer_b])
-    registry.handle_game_end(GameEnded(room_id=123))
-    spy_timer_a.reset.assert_called_once()
-    spy_timer_b.reset.assert_not_called()
+def test_turn_timer_registry_resets_proper_turn_timer_on_game_start(spy_timers):
+    registry = TurnTimerRegistry(spy_timers)
+    registry.handle_game_start(GameStarted(room_id=1))
+    spy_timers[0].reset.assert_called_once()
+    spy_timers[1].reset.assert_not_called()
+
+
+def test_turn_timer_registry_resets_proper_turn_timer_on_game_end(spy_timers):
+    registry = TurnTimerRegistry(spy_timers)
+    registry.handle_game_end(GameEnded(room_id=1))
+    spy_timers[0].reset.assert_called_once()
+    spy_timers[1].reset.assert_not_called()
 
 
 def test_turn_timer_registry_delegates_turn_started_event_to_proper_turn_timer(
-    spy_timer,
+    spy_timers,
 ):
-    spy_timer_a = spy_timer(123)
-    spy_timer_b = spy_timer(456)
-    registry = TurnTimerRegistry(turn_timers=[spy_timer_a, spy_timer_b])
-    registry.handle_turn_start(TurnStarted(room_id=123, player_id="player1"))
-    spy_timer_a.start.assert_called_once_with("player1")
-    spy_timer_b.start.assert_not_called()
+    registry = TurnTimerRegistry(spy_timers)
+    registry.handle_turn_start(TurnStarted(room_id=1, player_id="player1"))
+    spy_timers[0].start.assert_called_once_with("player1")
+    spy_timers[1].start.assert_not_called()
 
 
-def test_turn_timer_registry_cancels_turn_timer_on_turn_end(spy_timer):
-    spy_timer_a = spy_timer(123)
-    spy_timer_b = spy_timer(456)
-    registry = TurnTimerRegistry(turn_timers=[spy_timer_a, spy_timer_b])
-    registry.handle_turn_end(TurnEnded(room_id=123, player_id="player1"))
-    spy_timer_a.cancel.assert_called_once_with("player1")
-    spy_timer_b.cancel.assert_not_called()
+def test_turn_timer_registry_cancels_turn_timer_on_turn_end(spy_timers):
+    registry = TurnTimerRegistry(spy_timers)
+    registry.handle_turn_end(TurnEnded(room_id=1, player_id="player1"))
